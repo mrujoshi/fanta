98574aaaa32c713ce7c00abb1d2205ad47f9334f
Mrudula <mrudula.joshi@tomtom.com>
2025-05-03 22:20:10 +0530

    Initial commit

---
+++ src/main/java/com/fanta/fanta/FantaApplication.java
@@ -0,0 +1,13 @@
+package com.fanta.fanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fanta/fanta/FantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanta.fanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
